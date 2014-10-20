b6285be47a152261c3a3609d27e932ac24875217
hongmomanu <hongmomanu@gmail.com>
2014-10-20 17:23:27 +0800

    jack commit

--- resources/public/js/views/edituserpasswin.js
+++ resources/public/js/views/edituserpasswin.js
@@ -1,10 +1,11 @@
 define(function () {
 
     function render(parameters) {
+        $.parser.parse();
         $('#edituserpasswin').dialog({
             title: '修改密码窗口',
             width: 300,
-            height: 180,
+            height: 139,
             closed: false,
             cache: false,
             buttons:[{
@@ -12,27 +13,30 @@ define(function () {
                 id:'savenewpasswordbtn',
                 handler:function(){
                    //alert(1);
-                    require(['js/jqueryplugin/easyui-form.js','js/commonfuncs/AjaxForm.js']
-                        ,function(easyform,ajaxfrom){
+                    if($('#edituserpasswin form').form('validate')){
+                        require(['js/jqueryplugin/easyui-form.js','js/commonfuncs/AjaxForm.js']
+                            ,function(easyform,ajaxfrom){
 
 
-                            var params=$('#edituserpasswin form').form("serialize");
+                                var params=$('#edituserpasswin form').form("serialize");
 
-                            var success=function(res){
-                                if(res.success){
-                                    $.messager.alert('操作成功','修改密码成功!');
-                                    $('#edituserpasswin').dialog('close');
-                                }else{
+                                var success=function(res){
+                                    if(res.success){
+                                        $.messager.alert('操作成功','修改密码成功!');
+                                        $('#edituserpasswin').dialog('close');
+                                    }else{
+                                        $.messager.alert('操作失败','原始密码错误!');
+                                    }
+
+                                };
+                                var errorfunc=function(){
                                     $.messager.alert('操作失败','原始密码错误!');
                                 }
+                                ajaxfrom.ajaxsend('post','json','update-profile',params,success,null,errorfunc)
 
-                            };
-                            var errorfunc=function(){
-                                $.messager.alert('操作失败','原始密码错误!');
-                            }
-                            ajaxfrom.ajaxsend('post','json','update-profile',params,success,null,errorfunc)
+                            });
 
-                        });
+                    }
 
                 }
             },{
