415bc8fa58f147652cf9725d7f59772281565192
hongmomanu <hongmomanu@gmail.com>
2014-10-20 17:44:07 +0800

    jack commit

--- resources/public/js/views/individualregistrationpanel.js
+++ resources/public/js/views/individualregistrationpanel.js
@@ -3,7 +3,30 @@ define(function () {
 
 
     function render(parameters) {
+        $('#registedperson').datagrid({
+            singleSelect: true,
+            collapsible: true,
+            rownumbers: true,
+            method:'post',
+            url:'auth/getregistedperson',
+            remoteSort: false,
+            fitColumns:true,
+            fit:true,
+            //toolbar:'#enumpaneltb',
+            pagination:true,
+            pageSize:10,
+            onBeforeLoad: function (params) {
+                var options = $('#enummanagerpanel').datagrid('options');
+                params.start = (options.pageNumber - 1) * options.pageSize;
+                params.limit = options.pageSize;
+                params.totalname = "total";
+                params.rowsname = "rows";
+            },
+            onClickRow:function(index, rowData){
 
+            }
+
+        });
     }
 
     return {
