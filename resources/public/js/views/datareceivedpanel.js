8d3b08d2aa62dfe6419f5fd58038298fe0617073
hongmomanu <hongmomanu@gmail.com>
2014-11-03 10:29:36 +0800

    jack commit

--- resources/public/js/views/datareceivedpanel.js
+++ resources/public/js/views/datareceivedpanel.js
@@ -1,7 +1,29 @@
 define(function () {
 
     function render(parameters) {
+        $('#datareceivedpanel .checkingitems').datagrid({
+            singleSelect: true,
+            collapsible: true,
+            rownumbers: true,
+            method:'post',
+            url:'maintain/getregistedcheckitems',
+            remoteSort: false,
+            //fitColumns:true,
+            fit:true,
+            //toolbar:'#enumpaneltb',
+            pagination:false,
+            pageSize:300,
+            onBeforeLoad: function (params) {
+                var options = $(this).datagrid('options');
+                params.start = (options.pageNumber - 1) * options.pageSize;
+                params.limit = options.pageSize;
+                params.totalname = "total";
+                params.rowsname = "rows";
+            },
+            onClickRow:function(index, rowData){
+            }
 
+        });
 
     }
 
