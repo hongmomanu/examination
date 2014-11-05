ecd34547a3631f92482220f63ca6a4a1d566d4f6
hongmomanu <hongmomanu@gmail.com>
2014-11-05 22:04:29 +0800

    jack commit

--- resources/public/js/views/daysstaticpanel.js
+++ resources/public/js/views/daysstaticpanel.js
@@ -18,6 +18,24 @@ define(function () {
                     };
                     var succ=function(data){
 
+                        var personobj= data.person[0];
+                        var unitobj= data.unit[0];
+                        var div=$('#daysstaticpanel .content');
+                        div.html("");
+                        div.append("<a>日期："+$('#daysstaticpanel .beginday').datebox('getValue')+
+                            "-->"+$('#daysstaticpanel .endday').datebox('getValue')+
+                            "</a>");
+                        div.append('<table class="formtable">' +
+                            '<tr><td>个人人数：</td><td>'+personobj.counts+'</td>' +
+                            '<td>团体人数：</td><td>'+unitobj.counts+'</td>'+
+                            '<td>总人数：</td><td>'+(unitobj.counts+personobj.counts)+'</td>'+
+                            '</tr>' +
+                            '<tr><td>个人费用：</td><td>'+personobj.sums+'</td>' +
+                            '<td>团体费用：</td><td>'+unitobj.sums+'</td>'+
+                            '<td>总费用：</td><td>'+(unitobj.sums+personobj.sums)+'</td>'+
+                            '</tr>' +
+                            '</table>') ;
+
                         //console.log(data);
 
                     };
@@ -25,7 +43,7 @@ define(function () {
                         $.messager.alert('提示','error!');
                     };
 
-                    ajaxform.ajaxsend('post','json','maintain/getdaystatic',params,succ,null,errorfunc,true);
+                    ajaxform.ajaxsend('post','json','maintain/getdaystatic',params,succ,null,errorfunc,false);
 
 
 
