d4fba96aceb35ee473b53a7f9d2d99c8f40c3a6c
hongmomanu <hongmomanu@gmail.com>
2014-11-06 10:04:09 +0800

    jack commit

---
+++ resources/public/js/views/reportquerypanel.js
@@ -0,0 +1,47 @@
+define(function () {
+
+
+    function render(parameters) {
+
+        require(['js/jqueryplugin/easyui-form.js','js/commonfuncs/AjaxForm.js']
+            ,function(easyuiform,ajaxform){
+
+                $('#reportquerypanel .maintable').datagrid({
+                    singleSelect: true,
+                    collapsible: true,
+                    rownumbers: true,
+                    method:'post',
+                    url:'maintain/getregistedpersossssss',
+                    remoteSort: false,
+                    //fitColumns:true,
+                    fit:true,
+                    toolbar:'#reportquerypanel .searchtoolbar',
+                    pagination:true,
+                    pageSize:20,
+                    onBeforeLoad: function (params) {
+                        var options = $(this).datagrid('options');
+                        params.start = (options.pageNumber - 1) * options.pageSize;
+                        params.limit = options.pageSize;
+                        params.totalname = "total";
+                        params.rowsname = "rows";
+                    },
+                    onClickRow:function(index, rowData){
+
+                        //$('#unitcheckingitempanel .checkingitems').datagrid('load',{relationid:rowData.relationid});
+                    }
+
+                });
+
+
+            });
+
+
+
+
+    }
+
+    return {
+        render: render
+
+    };
+});
